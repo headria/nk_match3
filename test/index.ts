88df1cb56d50f6c2f54f893d4799ed967b98d24d
headria <headria@gmail.com>
2023-08-10 06:41:58 +0400

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,13 @@
 import { Client } from "@heroiclabs/nakama-js";
 import { v4 } from "uuid";
 import { encode } from "js-base64";
+import { ApiUpdateAccountRequest } from "@heroiclabs/nakama-js/dist/api.gen";
+
+var client = new Client("defaultkey", "nk.planetmemes.com", "443", true);
 
 const newUser = async (): Promise<void> => {
-  var client = new Client("defaulthttpkey", "127.0.0.1", "443");
-  const deviceId = await v4();
+  const deviceId = v4();
 
-  console.log(encode("defaulthttpkey" + ":"));
-  console.log(deviceId);
   try {
     await client.authenticateDevice(deviceId, true, "headria");
   } catch (err: any) {
@@ -22,4 +22,55 @@ const newUser = async (): Promise<void> => {
     );
   }
 };
-newUser();
+const updateAccount = async () => {
+  const deviceId = v4();
+  console.log(deviceId);
+  try {
+    const session = await client.authenticateDevice(deviceId);
+
+    const data: ApiUpdateAccountRequest = {
+      display_name: "Ahmad",
+      timezone: "GST",
+    };
+
+    const result = await client.updateAccount(session, data);
+    console.log(result);
+  } catch (err: any) {
+    console.log(
+      "Error authenticating device: %o:%o",
+      err.statusCode,
+      err.message,
+      err.bodyUsed,
+      err.status,
+      err.url,
+      err.body
+    );
+  }
+};
+
+const updateWallet = async () => {
+  const deviceId = v4();
+  console.log(deviceId);
+  try {
+    const session = await client.authenticateDevice(deviceId);
+
+    const data = {
+      address: "Ahmad",
+    };
+
+    var response = await client.rpc(session, "user/walletconnect", data);
+    console.log(response);
+  } catch (err: any) {
+    console.log(
+      "Error authenticating device: %o:%o",
+      err.statusCode,
+      err.message,
+      err.bodyUsed,
+      err.status,
+      err.url,
+      err.body
+    );
+  }
+};
+
+updateWallet();
