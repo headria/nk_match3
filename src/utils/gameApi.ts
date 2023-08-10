b5f633cc922a88a4f67a72b236d7881f8b8afa80
Mostafox <73644143+Mostafox@users.noreply.github.com>
2023-08-10 17:39:33 +0330

    Update gameApi.ts

--- src/utils/gameApi.ts
+++ src/utils/gameApi.ts
@@ -28,7 +28,6 @@ const GameApi = {
           {
             collection: this.Keys.collection,
             key: this.Keys.key,
-            version: "*",
             userId,
             value,
           },
@@ -47,17 +46,21 @@ const GameApi = {
       userId: string,
       data: LevelValidation.ILevelLog
     ): void {
-      nk.storageWrite([
-        {
-          collection: this.Keys.collection,
-          key: data.levelNumber.toString(),
-          userId,
-          version: "*",
-          value: data,
-          permissionRead: 2,
-          permissionWrite: 0,
-        },
-      ]);
+      try {
+        nk.storageWrite([
+          {
+            collection: this.Keys.collection,
+            key: data.levelNumber.toString(),
+            userId,
+            value: data,
+
+            permissionRead: 2,
+            permissionWrite: 0,
+          },
+        ]);
+      } catch (error: any) {
+        throw new Error(`failed to save LevelLog: ${error.message}`);
+      }
     }
     static get(nk: nkruntime.Nakama, userId: string, levelNumber: string) {
       const data = nk.storageRead([
@@ -72,21 +75,25 @@ const GameApi = {
     };
     static write(
       nk: nkruntime.Nakama,
+      levelNumber: number,
       userId: string,
-      levelLog: LevelValidation.ILevelLog,
       cheats: string[]
     ): void {
-      nk.storageWrite([
-        {
-          collection: this.Keys.collection,
-          key: levelLog.levelNumber.toString(),
-          userId,
-          value: { cheats, levelLog },
-          version: "*",
-          permissionRead: 2,
-          permissionWrite: 0,
-        },
-      ]);
+      try {
+        nk.storageWrite([
+          {
+            collection: this.Keys.collection,
+            key: levelNumber.toString(),
+            userId,
+
+            value: { cheats },
+            permissionRead: 2,
+            permissionWrite: 0,
+          },
+        ]);
+      } catch (error: any) {
+        throw new Error(`failed to save Cheats: ${error.message}`);
+      }
     }
   },
   Crypto: class {
