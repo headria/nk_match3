15f8e0653bced2cf5143a6e1007ee8cbead19a8a
Mostafox <73644143+Mostafox@users.noreply.github.com>
2023-09-09 18:36:36 +0330

    seperated configs

---
+++ src/00.configs/initialValues.ts
@@ -0,0 +1,30 @@
+const InitialWallet: Wallet.IWallet = {
+  Heart: {
+    endDate: 0,
+    isUnlimited: false,
+    quantity: 5,
+    next: 0,
+  },
+  TNT: {
+    endDate: 0,
+    isUnlimited: false,
+    quantity: 3,
+  },
+  DiscoBall: {
+    endDate: 0,
+    isUnlimited: false,
+    quantity: 3,
+  },
+  Rocket: {
+    endDate: 0,
+    isUnlimited: false,
+    quantity: 3,
+  },
+  Hammer: { quantity: 0 },
+  Shuffle: { quantity: 0 },
+  HorizontalRocket: { quantity: 0 },
+  VerticalRocket: { quantity: 0 },
+  Coins: { quantity: 1000 },
+  Gems: { quantity: 0 },
+  Score: { quantity: 0 },
+};
